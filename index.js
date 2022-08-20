07adba6bc725be9679c6696f56e86216062a9095
rodgersochieng <rodger.ochieng@student.moringaschool.com>
2022-08-20 13:54:29 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats =["Milo", "Otis", "Garfield"];
+ 
+function destructivelyAppendCat(tom){
+    cats.push(tom);
+}
+ 
+ function destructivelyPrependCat(tom){
+    cats.unshift(tom)
+ }
+ function destructivelyRemoveLastCat(){
+    cats.pop();
+ }
+
+   function destructivelyRemoveFirstCat(){
+    cats.shift()
+   }
+   function appendCat(jerry)
+   {return [...cats, jerry]}
+
+
+   function prependCat(jerry)
+   {return [jerry,...cats]}
+
+   function removeLastCat(){
+     return cats.slice(0,cats.length -1);
+   }
+
+   function removeFirstCat(){
+    return cats.slice(1);
+
+   }
+
+
